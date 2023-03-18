4a953c415e72967273e8b36b493501ac57524484
Yura Sorokin <yura.sorokin@percona.com>
2023-03-19 00:40:50 +0100

    Added Boost libraries integration (#1)

--- app.cpp
+++ app.cpp
@@ -1,6 +1,17 @@
+#include <cstdint>
 #include <iostream>
 
+#include <boost/json/src.hpp>
+
 int main(int /*argc*/, char * /*argv*/[]) {
+  boost::json::object obj;
+  obj["host"] = "127.0.0.1";
+  obj["port"] = static_cast<std::uint16_t>(3306);
+  obj["user"] = "root";
+  obj["password"] = "password";
+
   std::cout << "c++ standard version: " << __cplusplus << '\n';
+  std::cout << "serialized configuration: " << boost::json::serialize(obj)
+            << '\n';
   return 0;
 }
